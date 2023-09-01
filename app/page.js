f64b1af459427b65563f54cc689d7fa2e3e6efdc
Cesare10-dev <cesare1087@gmail.com>
2023-09-01 12:29:54 +0200

    params

--- app/page.js
+++ app/page.js
@@ -1,95 +1,105 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useState } from "react";
+import { useGlobalContext } from "@/app/context/global";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const router = useRouter();
+  const {
+    allPokemonData,
+    searchResults,
+    next,
+    getPokemon,
+    loading,
+    realTimeSearch,
+  } = useGlobalContext();
+
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+
+    realTimeSearch(search);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    realTimeSearch(search);
+  };
+
+  const displaySearchResults = () => {
+    return searchResults.map((pokemon) => {
+      return (
+        <div
+          key={pokemon.id}
+          onClick={() => {
+            router.push(`/pokemon/${pokemon.name}`);
+          }}
+          className="pokemon-name"
+        >
+          {pokemon.name}
         </div>
+      );
+    });
+  };
+
+  return (
+    <main>
+      <form action="" className="search-form" onSubmit={handleSearch}>
+        <div className="input-control">
+          <input
+            type="text"
+            value={search}
+            onChange={handleChange}
+            placeholder="Search for a Pokemon..."
+          />
+          <button className="submit-btn" type="submit">
+            Search
+          </button>
+        </div>
+      </form>
+
+      {search && searchResults.length > 0 && (
+        <div className="search-results">{displaySearchResults()}</div>
+      )}
+
+      <div className="all-pokemon">
+        {allPokemonData ? (
+          allPokemonData.map((pokemon) => {
+            return (
+              <div
+                key={pokemon.id}
+                className="card"
+                onClick={() => {
+                  router.push(`/pokemon/${pokemon.name}`);
+                }}
+              >
+                <div className="card-image">
+                  <img
+                    src={pokemon.sprites.other.home.front_default}
+                    alt={pokemon.name}
+                  />
+                </div>
+                <div className="card-body">
+                  <h3>{pokemon.name}</h3>
+                  <p>More Details &nbsp; &rarr;</p>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="next">
+        {allPokemonData.length > 0 && (
+          <button className="next-btn" onClick={next}>
+            Load More &darr;
+          </button>
+        )}
       </div>
     </main>
-  )
+  );
 }
